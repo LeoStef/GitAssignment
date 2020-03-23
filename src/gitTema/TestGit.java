61ee0f4156eabf009720fd807d81442d749cbe00
LeoStef <33711933+LeoStef@users.noreply.github.com>
2020-03-23 19:35:58 +0200

    Commit TestGit

---
+++ src/gitTema/TestGit.java
@@ -0,0 +1,11 @@
+package gitTema;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git ! Denumirea proiectului de licență "
+				+ "este Aplicație pentru recomandarea medicamentelor");
+	}
+
+}
