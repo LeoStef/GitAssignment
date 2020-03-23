6cf7ff8ab1cee3d165a3e497da8c7c31d018169a
LeoStef <33711933+LeoStef@users.noreply.github.com>
2020-03-23 19:58:58 +0200

    Clasa Medicament adaugata

---
+++ src/gitTema/Medicament.java
@@ -0,0 +1,14 @@
+package gitTema;
+
+public class Medicament {
+	String nume;
+	float pret;
+	
+	public Medicament(String nume, float pret) {
+		super();
+		this.nume = nume;
+		this.pret = pret;
+	}
+	
+	
+}
